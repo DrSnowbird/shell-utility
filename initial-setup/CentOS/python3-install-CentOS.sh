79acef884f9679fcaf91a438b3b6c3c68627aab8
openkbs <rsheu@openkbs.org>
2020-01-02 12:47:42 -0500

    update python3 CentOS setup

--- initial-setup/CentOS/python3-install-CentOS.sh
+++ initial-setup/CentOS/python3-install-CentOS.sh
@@ -2,6 +2,17 @@
 
 sudo yum install -y python36
 
+sudo ln -s /usr/bin/python3.6 /usr/bin/python3
+
+# Setup Default python version to python3
+python --version
+python3 --version
+
+sudo alternatives --remove python /usr/bin/python2
+sudo alternatives --set python /usr/bin/python3
+python --version
+python3 --version
+
 sudo yum install -y python36-devel
 
 sudo yum install -y python36-setuptools
--- initial-setup/CentOS/python3-with-virtualenv-install-CentOS.sh
+++ initial-setup/CentOS/python3-with-virtualenv-install-CentOS.sh
@@ -1,17 +1,26 @@
 #!/bin/bash -x
 
-#Step 1: Open a Terminal and add the repository to your Yum install.
+#Step-1: Open a Terminal and add the repository to your Yum install.
 sudo yum install -y https://centos7.iuscommunity.org/ius-release.rpm
 
-#Step 2: Update Yum to finish adding the repository.
+#Step-2: Update Yum to finish adding the repository.
 sudo yum -y update
 
-#Step 3: Download and install Python.
+#Step-3: Download and install Python.
 #This will not only install Python – but it will also install pip to help you with installing add-ons.
 sudo yum install -y python36u python36u-libs python36u-devel python36u-pip
 
 #Once these commands are executed, simply check if the correct version of Python has been installed by executing the following command:
 python3.6 -V
+
+#Step-3.1: Setup Default python version to python3
+python --version
+python3 --version
+
+sudo alternatives --remove python /usr/bin/python2
+sudo alternatives --set python /usr/bin/python3
+python --version
+python3 --version
 
 sudo pip3 install virtualenv
 sudo pip3 install virtualenvwrapper
