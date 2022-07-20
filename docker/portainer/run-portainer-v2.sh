1f6b856434cf899b5b76a613a9183609e0be9564
DrSnowbird <DrSnowbird@openkbs.org>
2022-07-20 19:57:31 -0400

    updated docker/portainer/run-portainer-v2.sh to print hint for web access URL

--- docker/portainer/run-portainer-v2.sh
+++ docker/portainer/run-portainer-v2.sh
@@ -9,3 +9,8 @@ docker run -d -p 8000:8000 -p 9443:9443 --name portainer-v2 \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v portainer_data:/data \
     portainer/portainer-ce:2.9.3
+
+echo -e
+echo -e ">>>>>> To access the new portainer:"
+echo -e "   https://localhost:9443/"
+echo -e
