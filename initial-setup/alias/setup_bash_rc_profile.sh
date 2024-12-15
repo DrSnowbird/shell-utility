a06f43e78dfebfed8fca3d5d166662f37a20b5b7
DrSnowbird <DrSnowbird@openkbs.org>
2024-12-15 16:52:12 -0500

    updated/moved shell scripts for Nvidia common problems

--- initial-setup/alias/setup_bash_rc_profile.sh
+++ initial-setup/alias/setup_bash_rc_profile.sh
@@ -4,8 +4,8 @@ cat <<EOF >> ~/.bashrc
 
 . ~/bin/git-alias.sh
 . ~/bin/docker-alias.sh
-. ~/bin/my-alias.sh
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64export 
-PATH=\${JAVA_HOME}:\${PATH}
+# . ~/bin/my-alias.sh
+# export JAVA_HOME=/usr/lib/jvm/java-18-openjdk-amd64export 
+PATH=\${PATH}:\${PATH}/bin
 
 EOF
